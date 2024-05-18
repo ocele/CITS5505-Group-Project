311f33cc145f90389aec0226a37c79f80ddcffd1
ocele <wyshzgsj@gmail.com>
2024-05-19 01:47:41 +0800

    Adjust Function

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,14 +1,14 @@
 $(function () {
     layui.use(['layer'], function () {
-        var layer = layui.layer; // 将 layer 初始化放在外面
+        var layer = layui.layer; 
 
         $(".upload_button").click(function () {
             $.ajax({
-                url: './upload.html?type=1', // 确认这个是你用来验证登录状态的API路径
+                url: './upload.html?type=1', 
                 method: 'GET',
                 success: function (data) {
                     console.log(data);
-                    if (data.code == 200 | !data.code) { // 假设成功时返回的状态码是200
+                    if (data.code == 200 | !data.code) { 
                         layer.open({
                             type: 2,
                             title: 'Submit Your Task',
@@ -17,15 +17,15 @@ $(function () {
                         });
                     } else {
                         layer.msg('Please log in first', {
-                            icon: 2, // 可以加上图标
-                            time: 2000 // 2秒后自动关闭
+                            icon: 2, 
+                            time: 2000 
                         });
                     }
                 },
                 error: function () {
                     layer.msg('Please log in first.', {
                         icon: 2,
-                        time: 2000 // 2秒后自动关闭
+                        time: 2000 
                     });
                 }
             });
